60316b6c6003c9459a1a2967be768f75c78a2b29
stridersol <network.ajay95@gmail.com>
2025-04-18 12:56:31 +0530

    Added example for Query, Path, Form Parameters

--- src/test/java/userManagement/getUsers.java
+++ src/test/java/userManagement/getUsers.java
@@ -6,6 +6,7 @@ import io.restassured.response.Response;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
 
 import java.util.Arrays;
 import java.util.List;
@@ -77,20 +78,20 @@ public class getUsers {
 
 	@Test
 	public void validateContainsInOrder() {
-	//Set base URI for the API
-	RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
-	
-	//Send a GET request and store the response in a variable
-	Response response = given()
-			.when()
-			.get("/comments?postId=1")
-			.then()
-			.extract()
-			.response();
-	
-	//Use Hamcrest to check that the response body contains specific items in a specific order
-	List<String> expectedEmails = Arrays.asList("Eliseo@gardner.biz","Jayne_Kuhic@sydney.com","Nikita@garfield.biz","Lew@alysha.tv","Hayden@althea.biz");
-	assertThat(response.jsonPath().getList("email"),contains(expectedEmails.toArray(new String[0])));
+		//Set base URI for the API
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		
+		//Send a GET request and store the response in a variable
+		Response response = given()
+				.when()
+				.get("/comments?postId=1")
+				.then()
+				.extract()
+				.response();
+		
+		//Use Hamcrest to check that the response body contains specific items in a specific order
+		List<String> expectedEmails = Arrays.asList("Eliseo@gardner.biz","Jayne_Kuhic@sydney.com","Nikita@garfield.biz","Lew@alysha.tv","Hayden@althea.biz");
+		assertThat(response.jsonPath().getList("email"),contains(expectedEmails.toArray(new String[0])));
 	}
 	
 	@Test
@@ -111,19 +112,80 @@ public class getUsers {
 		//Assert that the response contains 6 users
 		response.then().body("data", hasSize(6));
 		
-		//Assert that the first user in the list has the correct values
+		//Assert that the first user in the  list has the correct values
 		response.then().body("data[0].id",is(7));
 		response.then().body("data[0].email",is("michael.lawson@reqres.in"));
 		response.then().body("data[0].first_name",is("Michael"));
 		response.then().body("data[0].last_name",is("Lawson"));
-		response.then().body("data[0].avatar",is("https://reqres.in/img/faces/7-image.jpg"));
-
+		response.then().body("data[0].avatar",is("https://reqres.in/img/faces/7-image.jpg"));		
+	}
+	
+	@Test
+	public void validateStatusCodeUser() {
+		System.out.println("*********");
 		
+		Response resp = 
+				given()
+					.queryParam("page",2)
+					.when()
+					.get("https://reqres.in/api/users");
+		int actualStatusCode = resp.statusCode();
+		assertEquals(actualStatusCode,200);
+	}
+	
+	@Test
+	public void testGetUsersWithMultipleQueryParams() {
+		//Set base URI for the API
+		RestAssured.baseURI = "https://reqres.in/api";
 		
+		//Send a GET request and store the response in a variable
+		Response response = given()
+				.queryParam("page", 2)
+				.queryParam("per_page", 2)
+				.when()
+				.get("/users")
+				.then()
+				.statusCode(200)
+				.extract()
+				.response();	
+	}
+	
+	@Test
+	public void testGetUsersWithPathParams() {
+		String raceSeasonValue = "2017";
+		
+		//Set base URI for the API
+		RestAssured.baseURI = "http://ergast.com";
+		
+		//Send a GET request and store the response in a variable
+		Response response = given()
+				.pathParam("raceSeason",raceSeasonValue)
+				.when()
+				.get("/api/f1/{raceSeason}/circuits.json")
+				.then()
+				.statusCode(200)
+				.extract()
+				.response();
+		System.out.println(response.body().asString());
+	}
+	
+	@Test
+	public void testGetUsersWithFormParam() {
+		RestAssured.baseURI="https://reqres.in";
+		
+		Response response = 
+				given()
+				.contentType("application/x-www-form-urlencoded")
+				.formParam("name", "John Doe")
+				.formParam("job", "Developer")
+				.when()
+				.post("/users")
+				.then()
+				.statusCode(201)
+				.extract()
+				.response();
 				
-			
-		
-		
+		response.then().body("name", equalTo("John Doe"));
+		response.then().body("job",equalTo("Developer"));	
 	}
-
-	}
+}
